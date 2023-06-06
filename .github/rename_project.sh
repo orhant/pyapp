a5b402cc1243595f819c149a2481cea79b7ea193
orhant <orhant@users.noreply.github.com>
2023-06-06 20:37:50 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="orhant"
+original_name="pyapp"
+original_urlname="pyapp"
+original_description="Awesome pyapp created by orhant"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv pyapp $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
